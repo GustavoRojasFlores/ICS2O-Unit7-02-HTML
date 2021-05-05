17ecbe7db750f6c57a0d69a549fcabc350ec6b9c
Gustavo Rojas <56005777+GustavoRojasFlores@users.noreply.github.com>
2021-05-05 14:03:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 document.getElementById('btn').addEventListener('click', check)
 const text = document.getElementById('txt')
+const input = document.getElementById('inp')
 
 function check () {
-  const num = parseInt(document.getElementById('inp').value)
+  const num = parseInt(input.value)
   if (num * 0 === 0) {
     if (num / Math.abs(num) === 1) {
       text.innerHTML = 'The number ' + num + ' is a positive value.'
+      input.value = null
     }
     if (num / Math.abs(num) === -1) {
       text.innerHTML = 'The number ' + num + ' is a negative value.'
+      input.value = null
     }
     if (num === 0) {
       text.innerHTML = 'The number 0 is neither a positive nor a negative value.'
+      input.value = null
     }
+  } else {
+    text.innerHTML = 'Please enter a numerical value.'
+    input.value = null
   }
 }
